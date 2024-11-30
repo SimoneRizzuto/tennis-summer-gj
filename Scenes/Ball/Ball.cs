599cba73579764725812ded3b7a08dd224dbcd85
Simorico <simorico.dev@gmail.com>
2024-11-30 15:03:21 +1000

    minor clean up of commented code

--- Scenes/Ball/Ball.cs
+++ Scenes/Ball/Ball.cs
@@ -4,15 +4,11 @@ using TennisSummerGJ2024.UtilityClasses.Helpers;
 
 public partial class Ball : StaticBody2D
 {
-    // modify Mass, Friction or Bounce
+    public double CurrentHeight = 50;
     
+    private double heightDropSpeed = 2;
     private Shadow shadow;
     
-    
-    public double CurrentHeight = 50;
-    private double heightDropSpeed = 2;
-
-
     public override void _Ready()
     {
         shadow ??= GetNodeHelper.GetShadow(GetTree());
@@ -23,33 +19,7 @@ public partial class Ball : StaticBody2D
         
         Position = shadow.Position - new Vector2(0, (float)CurrentHeight);
         
-        //Position
-
-
-
         GD.Print("Ball - GLOBAL POSITION: " + GlobalPosition);
         GD.Print("Ball - POSITION: " + Position);
-
-        /*var currentHeight = Position.Y;
-
-        var newVec
-
-
-
-        var heightVector = new Vector2(0, -50);
-
-        Position -= heightVector * delta * heightDropSpeed;*/
-
-        //height -= height * delta * heightDropSpeed;
-
-
     }
-
-    /*public override void _IntegrateForces(PhysicsDirectBodyState2D state)
-    {
-        //LinearVelocity = new(0, -300);
-    }*/
-    
-    
-    /*;*/
 }
--- Scenes/Ball/Shadow.cs
+++ Scenes/Ball/Shadow.cs
@@ -14,6 +14,6 @@ public partial class Shadow : RigidBody2D
 
     public override void _Process(double delta)
     {
-        //Position = ball.Position - new Vector2(0, -(float)ball.CurrentHeight);
+        
     }
 }
