a8648d1fd2de8828acb22d6858e9d74e66d709e0
Simorico <simorico.dev@gmail.com>
2024-11-30 14:07:08 +1000

    ball bounces

--- Scenes/Ball/Ball.cs
+++ Scenes/Ball/Ball.cs
@@ -3,4 +3,21 @@ using System;
 
 public partial class Ball : RigidBody2D
 {
+    // modify Mass, Friction or Bounce
+    
+    
+    private double height = 50;
+    private double heightDropSpeed = 2; 
+
+    public override void _PhysicsProcess(double delta)
+    {
+        height -= height * delta * heightDropSpeed;
+        
+        
+    }
+
+    /*public override void _IntegrateForces(PhysicsDirectBodyState2D state)
+    {
+        //LinearVelocity = new(0, -300);
+    }*/
 }
