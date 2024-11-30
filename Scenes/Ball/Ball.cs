ecea781306934e5e0542da21e654ac3b1eff9218
Simorico <simorico.dev@gmail.com>
2024-11-30 15:15:23 +1000

    Ball lands on floor

--- Scenes/Ball/Ball.cs
+++ Scenes/Ball/Ball.cs
@@ -5,9 +5,12 @@ using TennisSummerGJ2024.UtilityClasses.Helpers;
 public partial class Ball : StaticBody2D
 {
     public double CurrentHeight = 50;
-    
+    private float DistanceToGround => GlobalPosition.DistanceTo(shadow.GlobalPosition);
+
     private double heightDropSpeed = 2;
     private Shadow shadow;
+    
+    
     
     public override void _Ready()
     {
@@ -16,10 +19,15 @@ public partial class Ball : StaticBody2D
 
     public override void _PhysicsProcess(double delta)
     {
-        
-        Position = shadow.Position - new Vector2(0, (float)CurrentHeight);
+        GlobalPosition = shadow.GlobalPosition - new Vector2(0, (float)CurrentHeight);
+
+        if (DistanceToGround > 5)
+        {
+            CurrentHeight -= 1;
+        }
         
         GD.Print("Ball - GLOBAL POSITION: " + GlobalPosition);
         GD.Print("Ball - POSITION: " + Position);
+        GD.Print("Distance To: " + DistanceToGround);
     }
 }
