5ffceb6aaa9d4c77fdd480ae7b5f47bf1bb89588
Simorico <simorico.dev@gmail.com>
2024-12-03 22:00:30 +1000

    Added logic for following ball on X axis

--- Scenes/Enemies/Enemy.cs
+++ Scenes/Enemies/Enemy.cs
@@ -1,5 +1,6 @@
 using Godot;
 using System;
+using TennisSummerGJ2024.UtilityClasses.Extensions;
 using TennisSummerGJ2024.UtilityClasses.Helpers;
 
 public partial class Enemy : CharacterBody2D
@@ -9,6 +10,8 @@ public partial class Enemy : CharacterBody2D
     private Ball ball;
     private Shadow shadow;
 
+    private int movementSpeed = 5000;
+
     private void FindNodes()
     {
         var tree = GetTree();
@@ -17,36 +20,54 @@ public partial class Enemy : CharacterBody2D
         shadow = GetNodeHelper.GetShadow(tree);
     }
     
+    public override void _Ready()
+    {
+        FindNodes();
+    }
+    
     public override void _Process(double delta)
     {
         FindNodes();
+    }
 
+    public override void _PhysicsProcess(double delta)
+    {
         switch (enemyState)
         {
             case EnemyState.Waiting:
                 ProcessWaiting();
                 break;
             case EnemyState.Repositioning:
-                ProcessRepositioning();
+                ProcessRepositioning(delta);
                 break;
             case EnemyState.Swinging:
                 ProcessSwinging();
                 break;
         }
         
+        MoveAndSlide();
     }
 
     private void ProcessWaiting()
     {
+        // if not on the back of the court, go there
+        
         if (shadow.LastHitByPlayer)
         {
             enemyState = EnemyState.Repositioning;
         }
     }
 
-    private void ProcessRepositioning()
+    private void ProcessRepositioning(double delta)
     {
-        Position = new(shadow.Position.X, Position.Y);
+        if (Math.Abs(Position.X - shadow.Position.X) < 10)
+        {
+            Velocity = Vector2.Zero;
+            return;
+        }
+        
+        var directionToBall = Position.DirectionTo(new (shadow.Position.X, Position.Y));
+        Velocity = directionToBall * movementSpeed * (float)delta;
     }
 
     private void ProcessSwinging()
