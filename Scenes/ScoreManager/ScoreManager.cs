58a64c27103f7543c7cc192a69a22a23ebd4af1c
Simorico <simorico.dev@gmail.com>
2024-12-04 21:01:37 +1000

    implemented score accurate to Tennis rules

--- Scenes/ScoreManager/ScoreManager.cs
+++ Scenes/ScoreManager/ScoreManager.cs
@@ -13,8 +13,23 @@ public partial class ScoreManager : Node2D
     private int playerScore = 0;
     private int opponentScore = 0;
 
+    private string PlayerDisplayScore => DisplayScore(playerScore);
+    private string OpponentDisplayScore => DisplayScore(opponentScore);
+
     public override void _Process(double delta)
     {
+        if (playerScore == 5)
+        {
+            GD.Print("GAME - Player Wins");
+            return;
+        }
+        
+        if (opponentScore == 5)
+        {
+            GD.Print("GAME - Opponent Wins");
+            return;
+        }
+        
         if (ScoreHasBeenTallied)
         {
             RespawnBall();
@@ -29,28 +44,71 @@ public partial class ScoreManager : Node2D
         {
             if (Shadow.OnPlayerSide)
             {
-                opponentScore += 15;
+                IncrementScore(Person.Opponent);
             }
             else
             {
-                playerScore += 15;
+                IncrementScore(Person.Player);
             }
         }
         else // Opponent's ball
         {
             if (Shadow.OnPlayerSide)
             {
-                opponentScore += 15;
+                IncrementScore(Person.Opponent);
             }
             else
             {
-                playerScore += 15;
+                IncrementScore(Person.Player);
             }
         }
         
-        GD.Print($"Player: {playerScore}, Opponent: {opponentScore}");
-
+        GD.Print($"Player: {PlayerDisplayScore}, Opponent: {OpponentDisplayScore}");
+        
         ScoreHasBeenTallied = true;
+    }
+
+    private void IncrementScore(Person person)
+    {
+        int calculatedScore;
+        
+        if (person == Person.Player)
+        {
+            if (opponentScore == 4)
+            {
+                opponentScore--;
+            }
+            else
+            {
+                playerScore++;
+            }
+        }
+        else if (person == Person.Opponent)
+        {
+            
+            if (playerScore == 4)
+            {
+                playerScore--;
+            }
+            else
+            {
+                opponentScore++;
+            }
+        }
+    }
+
+    private string DisplayScore(int score)
+    {
+        switch (score)
+        {
+            case 0: return "0";
+            case 1: return "15";
+            case 2: return "30";
+            case 3: return "40";
+            case 4: return "A";
+            case 5: return "G";
+            default: return "";
+        }
     }
 
     private void RespawnBall()
@@ -58,4 +116,10 @@ public partial class ScoreManager : Node2D
         Shadow.RespawnBall(GetTree());
         ScoreHasBeenTallied = false;
     }
+}
+
+public enum Person
+{
+    Player,
+    Opponent,
 }
