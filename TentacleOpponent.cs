5e80c6a5a080f93a167a4df2d1d7f0005927a6d1
Simone Rizzuto <simorico.dev@gmail.com>
2024-12-06 16:04:02 +1000

    done

--- TentacleOpponent.cs
+++ TentacleOpponent.cs
@@ -1,8 +1,18 @@
 using Godot;
 using System;
+using TennisSummerGJ2024.UtilityClasses.Helpers;
 
 public partial class TentacleOpponent : Enemy
 {
+	public override void GoToNextRound()
+	{
+		var scene = ResourceLoader.Load<PackedScene>("res://Scenes/VictoryScreen/VictoryScreen.tscn").Instantiate();
+		GetTree().CurrentScene.AddChild(scene);
+		
+		var gameRoom = GetNodeHelper.GetGameRoom(GetTree());
+		gameRoom.QueueFree();
+	}
+
 	public override void RestartRound()
 	{
 		VsScreen.SpawnScreen(GetTree().CurrentScene, "The Tentacle of the Lake", "res://Scenes/Enemies/TentacleOpponent.tscn", "res://Assets/Sprites/Tentacle Portrait.png");
